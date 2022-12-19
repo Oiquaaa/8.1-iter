be626f720eff822f6670fa6bd00bf7ffac502b13
FEYD <muson8842@gmail.com>
2022-12-19 16:36:36 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Введіть рядок: ";
+    getline(cin, s);
+
+    for (int i = 0; i < s.size() - 2; i++) {
+        if (s[i] == 'O' && s[i + 1] == 'G' && s[i + 2] == 'O') {
+            s[i] = '*';
+            s[i + 1] = '*';
+            s[i + 2] = ' ';
+        }
+        if (s[i] == 'A' && s[i + 1] == 'G' && s[i + 2] == 'A') {
+            s[i] = '*';
+            s[i + 1] = '*';
+            s[i + 2] = ' ';
+        }
+    }
+
+    cout << "Результат: " << s << endl;
+
+    return 0;
+}
